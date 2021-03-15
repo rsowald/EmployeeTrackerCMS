53cffa836a2312a18377407d4bf19b91b256d306
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-14 18:29:19 -0700

    initial inquirer menu

---
+++ employeeTracker.js
@@ -0,0 +1,55 @@
+const runMenu = async () => {
+    const answer = await inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all employees',
+            'View all roles',
+            'View all departments',
+            'Add employee',
+            'Add role',
+            'Add department',
+            'Update employee role',
+            'exit',
+        ]
+    });
+
+    switch (answer.action) {
+        case 'View all employees':
+
+            break;
+
+        case 'View all roles':
+
+            break;
+
+        case 'View all departments':
+
+            break;
+
+        case 'Add employee':
+
+            break;
+
+        case 'Add role':
+
+            break;
+
+        case 'Add department':
+
+            break;
+
+        case 'Update employee role':
+
+            break;
+
+        case 'Exit':
+            connection.end();
+            break;
+
+        default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+    }
+};
